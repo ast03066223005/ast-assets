06d1df5b733ef58e45ce5c7643843d38a1a9dd47
Abdulrehman <code.abdulrehman@gmail.com>
2025-09-26 00:15:18 +0500

    Update feConstants via FeConstantsStore

--- constants.js
+++ constants.js
@@ -1,16 +1,18 @@
 export const currency = "Rs.";
+
 export const whatsappNumber = "+923066223005";
-export const whatsappMessage = (product) => {
-  return `Hi! I want to order: ${product.title} - ${currency}:${product.discount_price} \n${window.location.href}`;
-};
-export const offerTimer = "2025-09-29T23:59:59";
+
+export const whatsappMessage = `Hi! I want to order: ${product.title} - ${currency}:${product.discount_price} \n${window.location.href}`;
+
+export const offerTimer = "2025-09-29T19:13:00.000Z";
+
 export const socialLinks = {
-  facebook: 'https://www.facebook.com/astelecom6',
-  twitter: 'https://twitter.com/',
-  instagram: 'https://instagram.com/',
-  linkedin: 'https://linkedin.com/company/',
-  youtube: 'https://youtube.com/',
-  tiktok: 'https://tiktok.com/',
-  whatsapp: 'https://wa.me/+923066223005',
-  telegram: 'https://t.me/'
+  "facebook": "https://www.facebook.com/astelecom6",
+  "twitter": "https://twitter.com/",
+  "instagram": "https://instagram.com/",
+  "linkedin": "https://linkedin.com/company/",
+  "youtube": "https://youtube.com/",
+  "tiktok": "https://tiktok.com/",
+  "whatsapp": "https://wa.me/+923066223005",
+  "telegram": "https://t.me/"
 };
